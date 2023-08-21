8c1240392bb0aa0d4df1c171815ea36adb5cc1ba
june0216 <ziyun1612@ewhain.net>
2023-08-22 01:41:13 +0900

    feat : 토큰 관련 예외 추가 토큰이 없을 때, 만료된 토큰일 때 예외 추가 Related to: #6

---
+++ kuddy-back/common/src/main/java/com/kuddy/common/security/exception/EmptyTokenException.java
@@ -0,0 +1,9 @@
+package com.kuddy.common.security.exception;
+
+import com.kuddy.common.exception.custom.UnAuthorizedException;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class EmptyTokenException extends UnAuthorizedException {
+}
