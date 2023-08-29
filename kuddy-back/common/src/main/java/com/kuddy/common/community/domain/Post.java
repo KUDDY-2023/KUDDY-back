c07f0cf6c3068aff915eb7d83fe68c437e638108
ParkIsComing <maroony55@gmail.com>
2023-08-29 19:48:37 +0900

    feat : 커뮤니티 게시글 관련 도메인 세팅
    Post, PostImage, ItinerarySpot 엔티티 및 관련 Enum 세팅
    
    Related to : #8

---
+++ kuddy-back/common/src/main/java/com/kuddy/common/community/domain/Post.java
@@ -0,0 +1,89 @@
+package com.kuddy.common.community.domain;
+
+import com.kuddy.common.community.exception.ExpiredDateException;
+import com.kuddy.common.community.exception.InvalidPostArguments;
+import com.kuddy.common.domain.BaseTimeEntity;
+import com.kuddy.common.member.domain.Member;
+import com.kuddy.common.spot.domain.District;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Post extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id", updatable = false)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Integer people; // only for PostType.JOIN_US
+
+    private LocalDate date; // only for PostType.JOIN_US
+
+    private District district; // only for PostType.JOIN_US
+
+    private PostType postType;
+
+    private String itinerarySpots; // only for PostType.ITINENARY
+
+    private Subject subject;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member author;
+
+    @Builder(builderMethodName = "JoinusBuilder", builderClassName = "JoinusBuilder")
+    public Post(String title, String content, Integer people, LocalDate date, District district, PostType postType,
+                Member author) {
+        validateJoinusPost(date, people, district);
+        this.title = title;
+        this.content = content;
+        this.people = people;
+        this.date = date;
+        this.district = district;
+        this.postType = postType;
+        this.author = author;
+    }
+
+    @Builder(builderMethodName = "OthersPostBuilder", builderClassName = "OthersPostBuilder")
+    public Post(String title, String content, PostType postType, Member author, Subject subject) {
+        this.title = title;
+        this.content = content;
+        this.postType = postType;
+        this.author = author;
+        this.subject = subject;
+    }
+
+    @Builder(builderMethodName = "ItinearyPostBuilder", builderClassName = "ItinearyPostBuilder")
+    public Post(String title, String content, PostType postType, String itinerarySpots, Member author) {
+        this.title = title;
+        this.content = content;
+        this.postType = postType;
+        this.itinerarySpots = itinerarySpots;
+        this.author = author;
+    }
+
+
+    //현재 날짜 기준으로 이미 지난 날짜에 대해서는 JoinUs 포스틩 작성 불가
+    private void validateJoinusPost(LocalDate date, Integer people, District district) {
+        LocalDate currentDate = LocalDate.now();
+
+        if (currentDate.isAfter(date)) {
+            throw new ExpiredDateException();
+        }
+
+        if (people == null || district == null) {
+            throw new InvalidPostArguments();
+        }
+    }
+}
