bbd35bcdedd34eba682f0dde49bd220075bec2ce
june0216 <ziyun1612@ewhain.net>
2023-09-01 03:03:40 +0900

    feat : repository 구현 meetup repository meetup 추가로 채팅 저장 repository 내용 추가 Related to: #15

--- kuddy-back/common/src/main/java/com/kuddy/common/chat/repository/MongoChatRepository.java
+++ kuddy-back/common/src/main/java/com/kuddy/common/chat/repository/MongoChatRepository.java
@@ -1,6 +1,7 @@
 package com.kuddy.common.chat.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +12,7 @@ import com.kuddy.common.chat.domain.mongo.Chatting;
 public interface MongoChatRepository extends MongoRepository<Chatting, String> {
 
 	List<Chatting> findByRoomId(Long roomId);
+	Optional<Chatting> findById(String id);
 
 	Page<Chatting> findByRoomIdOrderBySendDateDesc(Long roomId, Pageable pageable);
 }
