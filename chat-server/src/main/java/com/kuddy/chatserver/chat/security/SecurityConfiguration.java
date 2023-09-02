e21fa35a301fbe8c5bc68507068d26e3e9d65c2b
june0216 <ziyun1612@ewhain.net>
2023-09-03 01:33:39 +0900

    feat : 로그인 로직 분리 하나의 security config를 api 서버, chat 서버용으로 나눔 Related to: #35

---
+++ chat-server/src/main/java/com/kuddy/chatserver/chat/security/SecurityConfiguration.java
@@ -0,0 +1,94 @@
+package com.kuddy.chatserver.chat.security;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.kuddy.common.jwt.JwtAccessDeniedHandler;
+import com.kuddy.common.jwt.JwtEntryPoint;
+import com.kuddy.common.jwt.JwtFilter;
+import com.kuddy.common.security.repository.CookieAuthorizationRequestRepository;
+import com.kuddy.common.security.service.CustomOAuth2UserService;
+import com.kuddy.common.security.service.OAuth2AuthenticationFailureHandler;
+import com.kuddy.common.security.service.OAuth2AuthenticationSuccessHandler;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true) // preAuthorize 활성화
+@EnableWebSecurity  // Spring Security 설정 활성화
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+	private final JwtEntryPoint jwtEntryPoint;
+	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+	private final JwtFilter jwtFilter;
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+	private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+
+	private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
+
+	// Spring Security에서 제공하는 비밀번호 암호화 클래스.
+	// Service에서 사용할 수 있도록 Bean으로 등록한다.
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable() // Non-Browser Clients만을 위한 API 서버이므로, csrf 보호기능 해제
+			.headers().frameOptions().sameOrigin() // h2-console 화면을 보기 위한 처리.
+
+			.and()
+			.authorizeRequests() // URL 별로 리소스에 대한 접근 권한 관리
+			.anyRequest().permitAll()// 우선 다 허용
+			.and()
+			.cors().configurationSource(corsConfigurationSource()) // CorsConfigurationSource 를 cors 정책의 설정파일 등록
+
+			.and() // 시큐리티는 기본적으로 세션을 사용하지만, 우리는 세션을 사용하지 않기 때문에 Stateless로 설정
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+			.and()
+			// JwtFilter를 인증을 처리하는 UsernamePasswordAuthenticationFilter 전에 추가한다.
+			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+
+			.exceptionHandling() // 예외 처리 기능 작동
+			.authenticationEntryPoint(jwtEntryPoint) // 인증 실패시 처리
+			.accessDeniedHandler(jwtAccessDeniedHandler); // 인가 실패시 처리
+
+		return http.build();
+	}
+
+	// CORS 허용 적용
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedOrigin("http://localhost:3000");
+		configuration.addAllowedOrigin("https://kuddy.co.kr");
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
