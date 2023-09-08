43ff0157e9ca717ae5c81f6e5c01f0d225e3a9ee
kwonjiyun <ziyun1612@ewhain.net>
2023-09-08 16:03:19 +0900

    hotfix : DELETE mapping import 추가

--- chat-server/src/main/java/com/kuddy/chatserver/chat/controller/ChatController.java
+++ chat-server/src/main/java/com/kuddy/chatserver/chat/controller/ChatController.java
@@ -8,13 +8,7 @@ import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kuddy.common.chat.domain.Message;
